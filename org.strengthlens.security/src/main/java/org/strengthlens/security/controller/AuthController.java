e9a9cdd13e32bb2395d258690f8b67a220d2441b
xkushal <kushalyhakuri123@gmail.com>
2026-05-26 14:31:27 -0400

    Add OAuth2 security module and Redis cache config

--- org.strengthlens.security/src/main/java/org/strengthlens/security/controller/AuthController.java
+++ org.strengthlens.security/src/main/java/org/strengthlens/security/controller/AuthController.java
@@ -1,4 +1,20 @@
 package org.strengthlens.security.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/oauth2/v1")
 public class AuthController {
+
+    @GetMapping("/public")
+    public String publicApi(){
+        return "This is public api";
+    }
+
+    @GetMapping("/private")
+    public String privateApi(){
+        return "Authenticated";
+    }
 }
